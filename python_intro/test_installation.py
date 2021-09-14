1a9deb0c1d24c0e0512a3fd1c03da8d563085234
Tobias Doernbach <t.doernbach@cellumation.com>
2021-09-14 21:47:29 +0200

    added installation test script, updated gitignore

---
+++ python_intro/test_installation.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+import csv
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+import plotly.express as px
+import plotly.io as pio
+from sklearn import neighbors
+
+print('Installation successful!')
