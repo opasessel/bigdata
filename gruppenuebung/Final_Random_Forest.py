cff4fc66741c87616f0dbb67e20303253befca1d
OpaSessel <timboehmann@web.de>
2022-01-14 23:44:34 +0100

    Final V2 classified (get it?) Laufzeit könnte hoch sein mit den aktuellen Parametern

--- gruppenuebung/Final_Random_Forest.py
+++ gruppenuebung/Final_Random_Forest.py
@@ -5,63 +5,76 @@ from sklearn import ensemble
 from sklearn.metrics import f1_score
 
 
-def analyze_algo(algo_name, classifier, train, test):
-    """ Analyzes ML algorithm performance.
-    Create classifier during function call! """
+class OnlineShopAnalysis:
 
-    # select the last column as label (y)
-    X_train = train.iloc[:, :-1].values
-    y_train = train.iloc[:, -1].values
-    X_test = test.iloc[:, :-1].values
-    y_test = test.iloc[:, -1].values
+    def __init__(self, algo_name, classifier, grid_params):
+        self.algo_name = algo_name
+        self.classifier = classifier
+        self.grid_params = grid_params
 
-    # Training: Fit classifier to the training set
-    classifier.fit(X_train, y_train)
+        # read training and test data
+        self.train = pd.read_csv('online_shoppers_intention_train.csv')
+        self.test = pd.read_csv('online_shoppers_intention_test.csv')
 
-    # predict test set results
-    y_pred = classifier.predict(X_test)
+        # select the last column as label (y)
+        self.x_train = self.train.iloc[:, :-1].values
+        self.y_train = self.train.iloc[:, -1].values
+        self.x_test = self.test.iloc[:, :-1].values
+        self.y_test = self.test.iloc[:, -1].values
+
+    # Grid Search
+    def grid_search(self):
+        """ Perform parameter analysis via Grid Search & run analysis of algo with best parameters found """
+
+        # perform Grid Search
+        print('')
+        print('Starting Grid Search for', self.algo_name)
+        search = sklearn.model_selection.GridSearchCV(self.classifier, self.grid_params, cv=10, scoring='f1_macro')
+        search.fit(self.x_train, self.y_train)
+
+        # override classifier with best estimator
+        self.classifier = search.best_estimator_
+        print('Classifier was overridden with best estimator')
+        # set best params
+        self.best_params = search.best_params_
+        print('Best params:')
+        print(self.best_params)
+
+    def analyze_algo(self):
+        """ Analyzes ML algorithm performance.
+        Create classifier during function call! """
+        print('')
+        print('Starting Analysis with set classifier:')
+
+        # Training: Fit classifier to the training set
+        self.classifier.fit(self.x_train, self.y_train)
+
+        # predict test set results
+        self.y_pred = self.classifier.predict(self.x_test)
+
+        # calculate cross validation
+        self.cross_val = np.mean(
+            sklearn.model_selection.cross_val_score(self.classifier, self.x_train, self.y_train, cv=10, scoring='f1_macro'))
 
     # print the performance results for the predicted test results
-    print('')
-    print('Results for', algo_name, ':')
-    print('F1 Score:', f1_score(y_test, y_pred))
-    cross_val = np.mean(sklearn.model_selection.cross_val_score(classifier, X_train, y_train, cv=10, scoring='f1_macro'))
-    print('Cross Validation:', cross_val)
+    def print_result(self):
+        print('Results for', self.algo_name, ':')
+        print('F1 Score:', f1_score(self.y_test, self.y_pred))
+        print('Cross Validation:', self.cross_val)
 
 
-# Grid Search
-def grid_search(algo_name, classifier, param_grid, train, test):
-    """ Perform parameter analysis via Grid Search & run analysis of algo with best parameters found """
-
-    # select the last column as label (y)
-    X_train = train.iloc[:, :-1].values
-    y_train = train.iloc[:, -1].values
-
-    # Perform Grid Search
-    print('')
-    print('Starting Grid-Search for', algo_name)
-    search = sklearn.model_selection.GridSearchCV(classifier, param_grid, cv=10, scoring='f1_macro')
-    search.fit(X_train, y_train)
-
-    # Run analysis of algo with best parameters found
-    classifier_gs = search.best_estimator_
-    print('Starting Analysis with best params:')
-    print(search.best_params_)
-    analyze_algo(algo_name, classifier_gs, train, test)
-
-
-# read training and test data
-train = pd.read_csv('online_shoppers_intention_train.csv')
-test = pd.read_csv('online_shoppers_intention_test.csv')
-
-
-# Initiating Random Forest Classifier
+# initiating Random Forest Classifier
 R_classifier2 = ensemble.RandomForestClassifier()
-# Specifying parameters for Grid-Search
+# specifying parameters for Grid Search (defaults included)
 param_grid = {'n_estimators': [50, 100, 200],
               'max_depth': [10, 20, None],
-              'max_features': ['auto', 'int'],
               'min_samples_leaf': [1, 2, 4],
               'bootstrap': [True, False]}
-# Executing Grid-Search and Analysis
-grid_search('Random Forest', R_classifier2, param_grid, train, test)
+# executing the Onlineshop Analysis
+analysis1 = OnlineShopAnalysis('Random Forest', R_classifier2, param_grid)
+# start Grid Search
+analysis1.grid_search()
+# analyze
+analysis1.analyze_algo()
+# print the results
+analysis1.print_result()
